7ba851649e3cb6183715c699901c02f39378add8
PhucHB <81226356+PhucHB@users.noreply.github.com>
2021-06-01 17:03:13 +0700

    thay doi ket noi data

--- Web/Login.php
+++ Web/Login.php
@@ -13,7 +13,7 @@ require_once "config.php";
  $dbconn = pg_connect($conn_string);
 if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     
-    $hashpassword = md5($_POST['pwd']);
+    $hashpassword = md5($_POST['password']);
     $sql ="select *from public.user where email = '".pg_escape_string($_POST['email'])."' and password ='".$hashpassword."'";
     $data = pg_query($dbconn,$sql); 
     $login_check = pg_num_rows($data);
