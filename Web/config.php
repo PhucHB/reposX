4b837abd63c81ef57477b8aad86362fa1a025aa3
PhucHB <81226356+PhucHB@users.noreply.github.com>
2021-06-01 16:26:42 +0700

    thay doi ketnoi

--- Web/config.php
+++ Web/config.php
@@ -1,13 +1,13 @@
 <?php
-$conn_string = "host=ec2-54-211-176-156.compute-1.amazonaws.com"
-        . " port=5432 dbname=d7k35e83nt54kf"
-        . " user=lkblfujpnveoph"
-        . " password=a2dda41d2a64a9bc1de4ccc447742ff9c32185457810c259326670ce98c0fc42";
+$conn_string = "ec2-54-91-188-254.compute-1.amazonaws.com"
+        . " port=5432 dbname=daepclu8e7kfuk"
+        . " user=owksetzrdvlxcl"
+        . " password=b84953c9d2495fd42b3edf708d6d0a3334cdcec8e3b1b44c57a2032aeba1e491";
 $db = pg_connect($conn_string);
 //connect to a database named "test" on the host "sheep" with a username and password
    if(!$db) {
       echo "Error : Unable to open database\n";
    } else {
-      echo "Opened database successfully\n";
+      echo "Opened database successfully phuc\n";
    }
 ?>
