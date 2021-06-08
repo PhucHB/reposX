08cc0197127de45df4eb5f07d9307e217770f3b4
PhucHB <81226356+PhucHB@users.noreply.github.com>
2021-06-08 13:41:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,260 @@
 <!DOCTYPE html>
-<html>
-   <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Bootstrap Nav</title>
-      <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
-   </head>
-   <body>
-       <header>
-          <div class="container">
-            <div class="row"> 
-                <div class="col-md-4 col-lg-4">
-                    <img class="img-fluid" src="images/photo.jpg" width="1000px" height="200px"/>
-                </div>
-            </div>
-           </div>
-       </header>
-      <div class="container">
-         <ul class="nav">
+<html lang="en">
+<head>
+    <title>PPcycle</title>
+    <script src="https://kit.fontawesome.com/11fe009d3c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <!--css icon-->
+    <link rel="stylesheet" href="vendors/css/grad.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resources/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/3.6.95/css/materialdesignicons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="icon.css">
+
+</head>
+<body>
+   <header>
+      <a href=""><img class="logo" src="resources/img/logOASM.jpg" alt="photo"></a>
+   <ul class="main-nav">
+      <!--unorder list-->
+      <li><a href="Profile.html"><i class="fas fa-user-alt" style="font-size: 1cm;"></i></a></li>
+      <li><a href="#"><i class="fas fa-shopping-cart" style="font-size: 1cm;"></i></a></li>
+   </ul>
+   <div class="clearfix"></div>
+    <div class="row">
+       
+         <h1>Product quality and  <br>customer experience are our <br> top priorities <br></h1> <br>
+    </div>
+   </header>
+        
+        <hr>
+    
+         <ul class="nav nav-tabs nav-fill">
             <li class="nav-item">
-                <a class="nav-link" href="Web/Landing.html">Shopping</a>
+               <a class="nav-link" href="#">Trang Chủ</a>
+            </li>
+            <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                  Product
+                  </a>
+                  <div class="dropdown-menu">
+                     <a class="dropdown-item" href="plan.html">ALL product</a>
+                  </div>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">My cart</a>
+               <a class="nav-link" href="#">D......</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">Profile</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="Web/Register.php">Register</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="Web/Login.php">Login</a>
-            </li>
+                <a class="nav-link" href="#">.........</a>
+             </li>
              <li class="nav-item">
-                 <a class="nav-link" href="Web/LogOut.php">Logout</a>
+                <a class="nav-link" href="#">.........</a>
+             </li>
+             <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+               Tài khoảng
+               </a>
+               <div class="dropdown-menu">
+                   <a class="dropdown-item" href="Web/Login.php">Log-in</a>
+                   <a class="dropdown-item" href="Web/Register.php">Registor</a>
+                  <a class="dropdown-item" href="#">information</a>
+               </div>
             </li>
          </ul>
 
-      </div>
+     
+     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+     <hr>
+     <!--gioi thieu-->
+     <section class="about-section">
+        
+           
+           <h2>About us</h2> 
+           <p class="p-long">
+             <br> are a retail unit of imported vehicles and provide high quality ancillary equipment to consumers.
+            Our unit was established on 7/7/2020
+           </p>
+        
+        <div class="row">
+           <div class="col span-1-of-4 about picture">
+              <img src="resources/img/How-to-Improve-Teamwork-and-Collaboration-Skills0A.jpeg" width="230px" height="230px" alt="activity1">
+              <p class="picture-title">
+                  team working 
+              </p>
+           </div>
+           <div class="col span-2-of-4 about picture">
+            <img src="resources/img/images.png" width="230px" height="230px" alt="activity2">
+            <p class="picture-title">
+               support
+         </div>
+         <div class="col span-3-of-4 about picture">
+            <img src="resources/img/dich-vu-va-chat-luong-dich-vu-1.png" width="230px" height="230px" alt="activity3">
+            <p class="picture-title">
+               service 
+            </p>
+         </div>
+         <div class="col span-4-of-4 about picture">
+            <img src="resources/img/tải xuống.jpg"  width="230px" height="230px" alt="activity4">
+            <p class="picture-title">
+               all our friend
+            </p>
+         </div>
+           
+        </div>
+     </section>
+     <ul class="trangchu">
+        
+         <div>
+            <div class="container">
+               <div><h1>Contact Us</h1></div>
+               <br />
+               <div class="row">
+                   <div class="col-md-6">
+                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.834036683917!2d108.22842141478559!3d16.07409978887798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31421825f1045e4f%3A0xcc796e42a4979a!2zNjU4IE5nw7QgUXV54buBbiwgQW4gSOG6o2kgQuG6r2MsIFPGoW4gVHLDoCwgxJDDoCBO4bq1bmcgNTUwMDAwLCBWaWV0bmFt!5e0!3m2!1sen!2sus!4v1617354830212!5m2!1sen!2sus" width="500" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                       
+                   </div>
+                   <br />
+                   <div class="col-md-6">
+                       <form class="my-form">
+                           <div class="form-group">
+                               <label for="form-name">Name</label>
+                               <input type="email" class="form-control" id="form-name" placeholder="Name">
+                           </div>
+                           <div class="form-group">
+                               <label for="form-email">Email Address</label>
+                               <input type="email" class="form-control" id="form-email" placeholder="Email Address">
+                           </div>
+                           <div class="form-group">
+                               <label for="form-subject">Telephone</label>
+                               <input type="text" class="form-control" id="form-subject" placeholder="Subject">
+                           </div>
+                           <div class="form-group">
+                               <label for="form-message">Email your Message</label>
+                               <textarea class="form-control" id="form-message" placeholder="Message"></textarea>
+                           </div>
+                           <button class="btn btn-default" type="submit">Contact Us</button>                
+                       </form>
+                   </div>
+               </div>
+           </div>
+           
+           <style>
+               .my-form {
+                   color: #305896;
+               }
+               .my-form .btn-default {
+                   background-color: #305896;
+                   color: #fff;
+                   border-radius: 0;
+               }
+               .my-form .btn-default:hover {
+                   background-color: #4498C6;
+                   color: #fff;
+               }
+               .my-form .form-control {
+                   border-radius: 0;
+               }
+           </style>
+
+         </div>
+                
+     </ul>
+     <hr>
+     <ul class="trangchu">
+        <h2>San pham noi bac</h2>
+        <section>
+         <article>
+            <h1>YZF R1</h1>
+            <p>R1 2020 hàng nhập khẩu chính hãng</p>
+            <img src="https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2021/d7542f7e-9e4c-44f8-80bb-4c08ea2862b0.png" alt="photo" width="100%" height="300px" />
+            <div class="price"> 720,000,000 vnd </div>
+            <ul>
+               <li>Hỗ trả góp 0%</li>
+               <li>Bảo hành 3 năm hoặc 30,000 km</li>
+            </ul>
+            <button>Xem chi tiết</button> <br>
+            <button>Đặt mua</button>
+         </article>
+         <article>
+          <h1>EXCITER 155</h1>
+          <p>EXCITER 155 VVA 2020 hàng nhập khẩu chính hãng</p>
+          <img src="https://yamaha-motor.com.vn/wp/wp-content/uploads/2020/12/Exciter-155-GP-004.png" alt="photo" width="100%" height="300px"/>
+          <div class="price"> 50,000,000 VND </div>
+          <ul>
+             <li>Hỗ trả góp 0%</li>
+             <li>Bảo hành 3 năm hoặc 30,000 km</li>
+          </ul>
+          <button>Xem chi tiết</button> <br>
+          <button>Đặt mua</button>
+       </article>
+       <article>
+          <h1>YZF R3 </h1>
+          <p>R3 2020 hàng nhập khẩu chính hãng</p>
+          <img src="https://yamaha-motor.com.vn/wp/wp-content/uploads/2017/05/2020_YZF320-A_DPBMC_USA_360_026.png" alt="photo" width="100%" height="300px"/>
+          <div class="price"> 129,000,000 VND </div>
+          <ul>
+             <li>Hỗ trả góp 0%</li>
+             <li>Bảo hành 3 năm hoặc 30,000 km</li>
+          </ul>
+          <button>Xem chi tiết</button> <br>
+          <button>Đặt mua</button>
+       </article>
+      </section>
+     </ul>
+     <ul>
+        <div class="NEWS">
+         <h2>NEWS</h2>
+         <section>
+            <article >
+               <h1>khuyen mai nhan dip 30/4-1/5</h1>
+               <img src="http://nhaccuthanhthuy.com/wp-content/uploads/2018/10/giam-10.jpg" alt="photo" width="100%" height="150px"   />
+               <a href="#">nhan xem chi tiet</a>
+            </article>
  
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-   </body>
+         </section>
+ 
+        </div>
+     </ul>
+     <!--enfd -->
+     <div class="ending">
+      <div class="row">
+         <div class="col span-1-of-4">
+            <h3>can mua xe</h3>
+            <a href="#">tin bán xe mơi</a> <br>
+            <a href="#">xe cũ</a> <br>
+            <a href="#">trả góp</a>
+         </div>
+         <div class="col span-2-of-4">
+            <h3> can ban xe</h3>
+            <a href="#">định giá</a> <br>
+            <a href="#">đăng kí thời gian bán</a>
+        </div>
+        <div class="col span-3-of-4">
+         <h3> NEWS</h3>
+         <a href="#">khuyến mãi</a> <br>
+         <a href="#">chuuong trình sự kiện</a>
+     </div>
+
+        
+        
+
+     </div>
+     <hr>
+     <p>design by Phuc Hoang @123 </p>
+     <div class="icon">
+      <a href="https://www.facebook.com/profile.php?id=100041460853445"><i class="fab fa-facebook-square" style="font-size: 1cm;" ></i></a>
+      <a href="https://www.youtube.com/channel/UC0FBTL3WLVNUeTS3VZm1uCA"><i class="fab fa-youtube" style="font-size: 1cm;"></i></a>
+      <i class="fab fa-github" style="font-size: 1cm;"></i>
+     </div>
+     
+   </div>
+     
+     
+</body>
 </html>
