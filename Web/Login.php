334b05602a4b5bd905531af8619c9ccbf01c5b5f
PhucHB <81226356+PhucHB@users.noreply.github.com>
2021-06-08 13:27:05 +0700

    Update Login.php

--- Web/Login.php
+++ Web/Login.php
@@ -14,7 +14,7 @@ require_once "config.php";
 if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     
     $hashpassword = md5($_POST['password']);
-    $sql ="select *from public.user where email = '".pg_escape_string($_POST['email'])."' and password ='".$hashpassword."'";
+    $sql ="select *from public.User where email = '".pg_escape_string($_POST['email'])."' and password ='".$hashpassword."'";
     $data = pg_query($dbconn,$sql); 
     $login_check = pg_num_rows($data);
     if($login_check > 0){ 
